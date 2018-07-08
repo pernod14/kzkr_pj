8c4e5eb7eae30c93bbe4529c712a7c5e145f7222
Akihiro Ishimura <pernod1466@gmail.com>
2018-07-08 15:24:44 +0900

    tst

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    body = 'hoge'
     app.logger.info("Request body: " + body)
 
     # handle webhook body
